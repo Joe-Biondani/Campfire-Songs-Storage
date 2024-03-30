4f6a04db08ccb84cf3f345e03ea75e182789dd4c
Joe-Biondani <43620787+Joe-Biondani@users.noreply.github.com>
2024-03-30 21:53:36 +0000

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,316 @@
+var songs = [
+    [
+        "404",
+        "404",
+        "Song not found",
+        `<b>Are you sure that's the right link?</b>`,
+    ],
+    [
+        "Ging Gang Goolee",
+        "ging-gang-goolee",
+        "Sing Along",
+        `Ging Gang Goolee, Goolee, Goolee, Goolee Watcha Ging Gang Goo Ging Gang Goo<br />
+        Ging Gang Goolee, Goolee, Goolee, Goolee Watcha Ging Gang Goo Ging Gang Goo<br />
+        <br class="linebreak" />
+        Hayla, Hayla Shayla, Hayla Shayla Hayla Ho-o-o!<br />
+        Hayla, Hayla Shayla, Hayla Shayla Hayla Ho-o-o!<br />
+        <br class="linebreak"/>
+        Shalawally hallway shalawally shalawally!<br />
+        <br class="linebreak" />
+        Oompah, Oompah, Oompah, Oompah!`,
+    ],
+    [
+        "Boom Chicka Boom",
+        "boom-chicka-boom",
+        "Repeat After Me",
+        `I said a boom chicka boom<br />
+        I said a boom chicka boom<br />
+        I said a boom chicka rocka chicka rocka chicka boom<br />
+        Uh huh<br />
+        Oh yeah<br />
+        One more time<br />
+        [] style.`,
+    ],
+    [
+        "Quartermaster's Stores",
+        "qm-stores",
+        "Sing Along",
+        `There were [], []<br />
+        []<br />
+        In the stores, in the stores<br />
+        There were [], []<br />
+        In the quartermaster's stores (behind the door!)<br />
+        <br class="linebreak" />
+        My eyes are dim I cannot see<br />
+        I have not got my specs with me.<br />
+        <b>(Slow)</b> I have not got my specs with me.<br />
+        <br class="linebreak" />
+        <b>ALTERNATIVE:</b><br />
+        My eyes are dim I cannot see<br />
+        I've been at the pub since half past 3!<br />
+        <br class="linebreak" />
+        <b>Verse Ideas:</b><br />
+        "Rats, rats, big as cats<br />
+        "Mice, mice, running through the rice<br />
+        "Bread, bread, harder than your head<br />
+        "Scouts, Scouts, eating brussel sprouts<br />
+        "Foxes, foxes, stuffed in little boxes<br />
+        "Beans, beans, big as submarines`,
+    ],
+    [
+        "Everywhere We Go",
+        "everywhere-we-go",
+        "Repeat After Me",
+        `Everywhere we go-o!<br />
+        People always ask us!<br />
+        Who-o we are!<br />
+        And where we come from!<br />
+        So we tell them!<br />
+        We're from []/we're []!<br />
+        And if they can't hear us!<br />
+        We sing a little louder!/They must be deaf!`,
+    ],
+    [
+        "Never Get To Heaven",
+        "get-to-heaven",
+        "Repeat After Me & Sing Along",
+        `Oh you'll never get to heaven!<br />
+        []!<br />
+        Cos []!<br />
+        []!<br />
+        <br class="linebreak" />
+        Oh you'll never get to heaven [] cos [] []!<br />
+        I ain't gonna grieve, my lord no mooooore<br />
+        I ain't gonna shout, I ain't gonna swear, I ain't gonna pull my sister's hair,<br />
+        I ain't gonna grieieieve my Lord no mooooore!<br />
+        <br class="linebreak" />
+        <b>Verse Ideas:</b><br />
+        On roller skates, you'll roll right past those heavenly gates<br />
+        In []'s car, []'s car won't get that far<br />
+        On a jumbo jet, the Lord ain't built no runways yet<br />
+        On a land Scout's map, a land Scout's map is really crap/bad<br />
+        In a Girlguide's bra, a Girlguide's bra won't stretch that far<br />
+        In a wheeliebin, a wheeliebin got Sea Scouts in<br />
+        In a limousine, the Lord don't sell no gasoline<br />
+        On []'s boat, []'s boat won't even float<br />
+        On water skis, the Lord don't allow no hairy knees`,
+    ],
+    [
+        "Ina Clina Bird",
+        "ina-clina-bird",
+        "Repeat After Me",
+        `Early in the morning<br />
+        When you're fast asleep<br />
+        There's a little birdie<br />
+        With a funny beak<br />
+        And this little birdie<br />
+        Has a funny name<br />
+        It's the ina clina fleegle flougle out of a can of beer a bugle helter skelter fleegle flougle bird, bird`,
+    ],
+    [
+        "Have You Ever Been Fishing?",
+        "ever-been-fishing",
+        "Sing Along",
+        `<b>(Slow)</b>Have you<br />
+        <b>(Faster)</b>Ever been fishing on a bright sunny day<br />
+        All the little fishies swimming up and down the bay<br />
+        Put your hands in your pockets and your pockets in your pants<br />
+        All the little fishies do the oogie boogie dance<br />
+        <br class="linebreak" />
+        Nanana, nanana, put your hands in your pockets<br />
+        And your pockets in your pants<br />
+        All the little fishies do the oogie boogie dance.`,
+    ],
+    [
+        "Pirate Song",
+        "pirate-song",
+        "Sing Along",
+        `When I was [] I []<br />
+        The day I went to sea<br />
+        I climbed aboard a pirate ship<br />
+        And the captain said to me<br />
+        <br class="linebreak" />
+        We're going<br />
+        This way<br />
+        That way<br />
+        Over the Irish sea<br />
+        A bottle of rum<br />
+        To fill me tum<br />
+        And that's the life for me`,
+    ],
+    [
+        "Joe's Button Factory",
+        "button-factory",
+        "Spoken & Actions",
+        `Hey<br />
+        My name is Joe<br />
+        I've got a wife and threeeeee kids<br />
+        And one day<br />
+        Me boss said to me<br />
+        Ey Joe<br />
+        You busy?<br />
+        And I said<br />
+        Nah/no/not really<br />
+        He said Joe<br />
+        Push this button with your []`,
+    ],
+    [
+        "Bungalow",
+        "bungalow",
+        "Group Repeat & Actions",
+        `<b>Campfire Leader</b><br />
+        <b>(point to someone!)</b><br />
+        Hey you!<br />
+        Hey you!<br />
+        Show us how you bungalow, show us how you bungalow!<br />
+        <br class="linebreak" />
+        <b>Them</b><br />
+        My hands are high<br />
+        My feet are low<br />
+        And this is how I bungalow<br />
+        <br class="linebreak" />
+        <b>All</b><br />
+        Their hands are high<br />
+        Their feet are low<br />
+        And this is how they bungalow!`
+    ],
+    [
+        "I Saw A Bird",
+        "saw-a-bird",
+        "Repeat After Me",
+        `I saw a bird,<br />
+        With a yellow bill,<br />
+        It landed on,<br />
+        My windwsil<br />
+        I lured him in,<br />
+        With a [],<br />
+        And then I [],<br />
+        []<br />
+        <br class="linebreak" />
+        <b>Ideas</b><br />
+        Bread, chopped off his head<br />
+        Cheese, hacked off his knees<br />
+        Trifle, shot him with a rifle<br />
+        One Direction poster, put him in the toaster<br />
+        Can of tiser, put him in a liquidiser`
+    ],
+    [
+        "Johnny Was A Parachuter",
+        "johnny",
+        "Sing Along & Actions",
+        `I am not typing this all out - you've just got to know it lol`
+    ],
+    [
+        "Ain't No Flies On Us",
+        "flies-on-us",
+        "Competition",
+        `There ain't no flies on us!<br />
+        There ain't no flies on us!<br />
+        There may be flies on some of you guys!<br />
+        But there ain't no flies on us!`
+    ],
+    [
+        "Crazy Moose",
+        "crazy-moose",
+        "Repeat After Me",
+        `There was a crazy moose,<br />
+        Who liked to drink a lot of juice<br />
+        There was a crazy moose,<br />
+        Who liked to drink a lot of juice.<br />
+        <br class="linebreak" />
+        Singing way-oh, way-oh<br />
+        Way-oh way-oh way-oh way-oh<br />
+        Way-oh way-oh<br />
+        Way-oh way-oh way-oh way-og<br />
+        <br class="linebreak" />
+        The moose's name was fred.<br />
+        He liked to drink his juice in bed<br />
+        <br class="linebreak" />
+        He drank his juice with care<br />
+        But he spilt some in his hair<br />
+        <br class="linebreak" />
+        All his hair went stiff<br />
+        So he combed it in a quiff<br />
+        <br class="linebreak" />
+        All his friends did laugh<br />
+        So he had to take a bath<br />
+        <br class="linebreak" />
+        As the water went down<br />
+        Fred the moose began to drown<br />
+        Singing glug-glug-glug-glug<br />
+        <br class="linebreak" />
+        Now let's all count to five<br />
+        Fred the moose is back alive`
+    ],
+    [
+        "Penguin Song",
+        "penguin",
+        "Sing Along & Actions",
+        `Have you ever seen<br />
+        A penguin come to tea<br />
+        Take a look at me<br />
+        A penguin you will see<br />
+        Penguins attention!<br />
+        Penguins salute!<br />
+        []`
+    ],
+    [
+        "Forty Years On An Iceberg",
+        "forty-years-iceberg",
+        "Repeat After Me & Actions",
+        `Forty years on an iceberg<br />
+        Over the ocean wide<br />
+        Nothing to wear but pyjamas<br />
+        Nothing to do but slide<br />
+        The wind was cold and icy<br />
+        The frost began to bite<br />
+        I had to hug a polar bear<br />
+        To keep me warm at night.<br />
+        <br class="linebreak" />
+        <b>Repeat getting faster or do as a sing along and do similar to B.I.N.G.O (hum lines)</b>`
+    ],
+    [
+        "Alice The Camel",
+        "alice-camel",
+        "Sing Along",
+        `Alice the camel has [10] humps<br />
+        Alice the camel has [] humps<br />
+        Alice the camel has [] humps<br />
+        So go Alice go<br />
+        Bum bum bum<br />
+        <br class="linebreak" />
+        <b>Continue down from 10 until none:</b><br />
+        Alice the camel has no humps<br />
+        Alice the camel has no humps<br />
+        Alice the camel has no humps<br />
+        'Cause Alice is a HORSE!`
+    ],
+    [
+        "Three Blind Jellyfish",
+        "blind-jellyfish",
+        "Story & Sing Along & Actions",
+        `[] blind jellyfish<br />
+        [] blind jellyfish<br />
+        [] blind jellllllyfish,<br />
+        sitting<br />
+        on<br />
+        a rock<br />
+        <br class="linebreak" />
+        <b>Repeat and make a story that's funny. Make sad bits very dramatic etc.</b>`
+    ],
+    [
+        "Grand Old Duke Of York",
+        "duke-of-york",
+        "Sing Along & Actions",
+        `Oh the grand old duke of York<br />
+        He had ten thousand men<br />
+        He marched them up to the top of the hill<br />
+        And he marched them down again<br />
+        And when they were up they were up<br />
+        And when they were down they were down<br />
+        And when they were only halfway up<br />
+        They were neither up nor down<br />
+        <br class="linebreak" />
+        <b>Stand up/sit down/hover as appropriate - switch around at some point too</b>`
+    ],
+];
